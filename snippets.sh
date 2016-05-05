c754140c498d444261ff72277d4411635be28b58
r82 <>
2016-05-05 11:02:19 +0200

    ''

--- snippets.sh
+++ snippets.sh
@@ -2,4 +2,4 @@
 find -type f | egrep -i "[.]php|js|css|htm|html|xml|xls$" | tar -czvf backup$(date +"%Y-%m-%d_%H;%M;%S").tar.gz -T -
 
 # find string ( I - ignore binary )
-grep -rI --include='*.*' -F 'string-to-find'
+grep --color -rI --include='*.*' -F 'string-to-find'

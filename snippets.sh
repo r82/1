ca493efeb4c42de6efa651ed2ee171bd3747ff91
r82 <>
2016-05-05 09:31:51 +0200

    ''

--- snippets.sh
+++ snippets.sh
@@ -1,2 +1,5 @@
 # backup
 find -type f | egrep -i "[.]php|js|css|htm|html|xml|xls$" | tar -czvf backup$(date +"%Y-%m-%d_%H;%M;%S").tar.gz -T -
+
+# find string ( I - ignore binary )
+grep -rI --include='*.c' -F 'toUpperCase('

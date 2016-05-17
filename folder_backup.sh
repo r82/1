4e63462d6ee4299e1e57fffd07eea574b28d90e9
r82 <>
2016-05-17 16:18:19 +0200

    ''

--- folder_backup.sh
+++ folder_backup.sh
@@ -3,7 +3,7 @@
 # parameters
 # 1 - destination folder
 # 2 - source folder, files that we are going to save
-# 3 - regex used to catch file names, if == <only_text> then files that are text-only are stored
+# 3 - regex used to catch file names, if == <guess_text_files_by_content> then files that are text-only are stored
 # 4 - date format
 # all parameters are optional
 
@@ -25,16 +25,26 @@ backup_dst="./"
 if [ ! -z ${1+x} ]; then
 	backup_dst=$1
 fi
-backup_dst=${backup_dst%/}/
+
+if [ ${backup_dst:0:1} != "/" ]; then
+	backup_dst=$PWD"/"$backup_dst
+fi
 if [ ! -d "$backup_dst" ]; then
   mkdir -p "$backup_dst"
 fi
+backup_dst=$(realpath -e $backup_dst)
+backup_dst=${backup_dst%/}/
+echo "backup_dst: $backup_dst"
+echo "backup_src: "$(realpath -e $backup_src)"/"
 
 # http://www.file-extensions.org/filetype/extension/name/text-files
-# backup_regex=".*"
-backup_regex="[.](php|js|css|htm|html|xml|xls|[.]htaccess|log|svg|sh|txt|sql|md|json|ini|inf|operations|tmpl|po|pot|mo|less|[.]editorconfig|crt|ts|[.]gitignore|dist|htc|yml)$"
+backup_regex=".*"
 if [ ! -z ${3+x} ]; then
 	backup_regex=$3
+fi
+
+if [ "$backup_regex" = "<guess_text_files_by_ext>" ]; then
+	backup_regex="[.](php|js|css|htm|html|xml|xls|[.]htaccess|log|svg|sh|txt|sql|md|json|ini|inf|operations|tmpl|po|pot|mo|less|[.]editorconfig|crt|ts|[.]gitignore|dist|htc|yml)$"
 fi
 
 backup_date_format="%Y-%m-%d_%H%M%S"
@@ -46,8 +56,14 @@ backup_name=${PWD##*/}
 
 cd $backup_src
 
-if [ $backup_regex = "<only_text>" ]; then
-	find . -type f -exec grep --exclude="*.pdf" -Iq . {} \; -and -print | tar -czvf $backup_dst"backup_"$backup_name"_"$(date +$backup_date_format).tar.gz -T -
+backup_file_name=$backup_dst"backup_"$backup_name"_"$(date +$backup_date_format).tar.gz
+backup_file_name_UNFINISHED=$backup_file_name".UNFINISHED"
+
+if [ "$backup_regex" = "<guess_text_files_by_content>" ]; then
+	# it's mistakenly taking *.pdf files as text
+	find . -type f -exec grep --exclude="*.pdf" -Iq . {} \; -and -print | tar -czvf "$backup_file_name_UNFINISHED" -T -
 else
-	find -type f | egrep -i $backup_regex | tar -czvf $backup_dst"backup_"$backup_name"_"$(date +$backup_date_format).tar.gz -T -
+	find . -type f | egrep -i "$backup_regex" | tar -czvf "$backup_file_name_UNFINISHED" -T -
 fi
+
+mv "$backup_file_name_UNFINISHED" "$backup_file_name"

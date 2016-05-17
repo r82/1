960d006c1b7b3f82366eefb9879ee03814af3b81
r82 <>
2016-05-17 13:06:47 +0200

    ''

--- snippets.sh
+++ snippets.sh
@@ -1,5 +1,5 @@
 # backup
-find -type f | egrep -i "[.]php|js|css|htm|html|xml|xls$" | tar -czvf backup$(date +"%Y-%m-%d_%H;%M;%S").tar.gz -T -
+find -type f | egrep -i "[.](php|js|css|htm|html|xml|xls)$" | tar -czvf backup$(date +"%Y-%m-%d_%H;%M;%S").tar.gz -T -
 
 # find string ( I - ignore binary )
 grep --color -rI -F 'string-to-find'
